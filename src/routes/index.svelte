db20d4845a94c1ee8355913628f79c01d175d4b2
sw-yx <shawnthe1@gmail.com>
2021-04-26 05:50:42 +0800

    ajaxify form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,61 @@
 <script context="module">
-	export const prerender = true;
+  export const prerender = true;
 </script>
 
+<script>
+  let isSubmitting = false;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let myForm = document.getElementById("test");
+    let formData = new FormData(myForm);
+    isSubmitting = true;
+    return fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => {
+        console.log("Form successfully submitted");
+        isSubmitting = false;
+        myForm.reset();
+      })
+      .catch((error) => {
+        alert(error);
+        isSubmitting = false;
+      });
+  };
+</script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
 
 <!-- "/src/routes/test-form.svelte" -->
-<form name="test" method="post" netlify netlify-honeypot="bot-field">
-    <input type="hidden" name="form-name" value="test" />
-    <input type="text" name="bot-field" />
-    <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-        <label>Message: <textarea name="message" /></label>
-    </p>
-    <p>
-        <button type="submit">Send</button>
-    </p>
+<form
+  id="test"
+  name="test"
+  on:submit={handleSubmit}
+  netlify
+  netlify-honeypot="bot-field"
+>
+  <input type="hidden" name="form-name" value="test" />
+  <input type="text" name="bot-field" style="opacity: 0" />
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message" /></label>
+  </p>
+  <p>
+    {#if isSubmitting}
+      <div>Submitting</div>
+    {:else}
+      <button type="submit">Send</button>
+    {/if}
+  </p>
 </form>
